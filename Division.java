f2c72410803b1a881d5eabe4993051b66afe6840
NikshepaXworkZ <nikshepaxworkz@gmail.com>
2023-08-22 11:00:06 +0530

    examples for methods

---
+++ Division.java
@@ -0,0 +1,37 @@
+class Division {
+ public static void main (String args[]) {
+		   System.out.println("main started");
+            int total= div(22,58);
+			System.out.println(total);
+            int total0= div(128,78);
+			System.out.println(total0);
+			int total1= div(22,588);
+			System.out.println(total1);
+			int total2= div(554,55);
+			int total= div(2231,252);
+			int total= div(45,32);
+			int total= div(897,457);
+			int total= div(65,889);
+			int total= div(45,52);
+			int total= div(78,85);
+			int total= div(52,12);
+			int total= div(656,321);
+			int total= div(789,654);
+			int total= div(23,17);
+			int total= div(45,21);
+			int total= div(784,855);
+			int total= div(562,44);
+			int total= div(785,632);
+			int total= div(551,489);
+			int total= div(22,33);
+				System.out.println(total);
+ 
+ }
+ public static int div(int a, int b){
+	 int c = a/b;
+	// int total=0;
+	// System.out.println(a/b);
+	// return total;
+	return c;
+ }
+}
